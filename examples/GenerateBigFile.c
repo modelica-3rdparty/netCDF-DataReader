ae6544151b9b9674c23f9f923d7728946b899bd1
Joerg Raedler <joerg@j-raedler.de>
2012-05-15 17:49:46 +0200

    corrected number of points

--- examples/GenerateBigFile.c
+++ examples/GenerateBigFile.c
@@ -61,7 +61,7 @@ void createBigFile(char *fileName) {
 
 
 int main(void) {
-    printf("This generates a very big file with %d data points.\n", DIM);
+    printf("This generates a very big file with %d data values.\n", DIM*(VARS+1));
     printf("Make sure you have enough disk space and some time.\n");
     printf("Enter 'y' to proceed, any other to stop now: ");
     if (getchar() == 'y') {
